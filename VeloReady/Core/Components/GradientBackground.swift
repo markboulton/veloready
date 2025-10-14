5eb393ca3d1d9b69e8b5523f189dba022f4b554f
Mark Boulton <mark@markboulton.co.uk>
2025-10-14 07:45:41 +0100

    design: Remove gradients - use system background (black/white)

--- VeloReady/Core/Components/GradientBackground.swift
+++ VeloReady/Core/Components/GradientBackground.swift
@@ -1,31 +1,9 @@
 import SwiftUI
 
-/// Reusable dual gradient background (two shades of blue)
+/// Simple background - black in dark mode, white in light mode
 struct GradientBackground: View {
     var body: some View {
-        ZStack {
-            // Lighter blue gradient from top-left
-            RadialGradient(
-                gradient: Gradient(colors: [
-                    Color.blue.opacity(0.15),
-                    Color.clear
-                ]),
-                center: .topLeading,
-                startRadius: 0,
-                endRadius: UIScreen.main.bounds.height * 0.7
-            )
-            
-            // Deeper blue gradient from top-right
-            RadialGradient(
-                gradient: Gradient(colors: [
-                    Color.blue.opacity(0.25),
-                    Color.clear
-                ]),
-                center: .topTrailing,
-                startRadius: 0,
-                endRadius: UIScreen.main.bounds.height * 0.7
-            )
-        }
-        .ignoresSafeArea()
+        Color(.systemBackground)
+            .ignoresSafeArea()
     }
 }
